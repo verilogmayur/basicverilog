d9dd29c01d66de909c4e816d6ec060ece1df9f4e
verilogmayur <143901239+verilogmayur@users.noreply.github.com>
2023-09-02 23:50:13 +0530

    Update 2_1_mux_tb.v

--- 2_1_mux_tb.v
+++ 2_1_mux_tb.v
@@ -1,22 +1,22 @@
-//Asimpletestbench
+//A simple testbench
 module testbench;
 reg sel;
 reg [1:0]mux_in;
-Moduleinstantiationbynameassociation
+//Moduleinstantiationbynameassociation
 mux2to1u0(.mux_out(mux1_out),.sel(sel),
 mux_in(mux_in[1:0]));
-Inputpatternsexhaustivelygenerated
+//Input patterns exhaustively generated
 initialbegin
-#10sel=0;mux_in[1:0]=2’b00; //Firstinputsignal
-#10sel=0;mux_in[1:0]=2’b01; //Secondinputsignal
-#10sel=0;mux_in[1:0]=2’b10; //Thirdinputsignal
-#10sel=0;mux_in[1:0]=2’b11; //Fourthinputsignal
-#10sel=1;mux_in[1:0]=2’b00; //Fifthinputsignal
-#10sel=1;mux_in[1:0]=2’b01; //Sixthinputsignal
-#10sel=1;mux_in[1:0]=2’b10; //Seventhinputsignal
-#10sel=1;mux_in[1:0]=2’b11; //Eighthinputsignal
+#10 sel=0;mux_in[1:0]=2’b00; //Firstinputsignal
+#10 sel=0;mux_in[1:0]=2’b01; //Secondinputsignal
+#10 sel=0;mux_in[1:0]=2’b10; //Thirdinputsignal
+#10 sel=0;mux_in[1:0]=2’b11; //Fourthinputsignal
+#10 sel=1;mux_in[1:0]=2’b00; //Fifthinputsignal
+#10 sel=1;mux_in[1:0]=2’b01; //Sixthinputsignal
+#10 sel=1;mux_in[1:0]=2’b10; //Seventhinputsignal
+#10 sel=1;mux_in[1:0]=2’b11; //Eighthinputsignal
 end
-//Displayresultswhenevermonitoredsignalschange
+//Display results whenever monitored signals change
 initial
 $monitor ($stime,"out=%b,sel=%b,in=%b",
 mux1_out,sel,mux_in);
